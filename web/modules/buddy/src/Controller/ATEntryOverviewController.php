fa992f34ec68d4da5914154a835014418be06c40
Peter Heumader <peter.heumader@gmail.com>
2021-05-25 12:43:37 +0200

    ^removed old at entry overview

--- web/modules/buddy/src/Controller/ATEntryOverviewController.php
+++ web/modules/buddy/src/Controller/ATEntryOverviewController.php
@@ -1,24 +1,23 @@
 <?php
+
 namespace Drupal\buddy\Controller;
 
 use Drupal\buddy\Util\Util;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
 
 class ATEntryOverviewController extends ControllerBase
 {
 
 
-  public function ATEntryOverview(){
+  public function ATEntryOverview()
+  {
 
     //throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
 
     $user = \Drupal::currentUser();
-
-
     $atEntriesID = \Drupal::entityQuery('node')
       ->condition('type', 'at_entry')
       ->condition('uid', $user->id(), '=')
@@ -30,7 +29,7 @@ class ATEntryOverviewController extends ControllerBase
 
     $atDescriptionsOfATEntries = [];
     $atPlatformsOfATEntries = [];
-    foreach ($atEntries as $atEntry){
+    foreach ($atEntries as $atEntry) {
       $atDescriptionIDs = $atEntry->field_at_descriptions->getValue();
       $atDescriptions = Util::loadNodesByReferences($atDescriptionIDs);
       $atDescriptionsOfATEntries[$atEntry->id()] = $atDescriptions;
@@ -41,26 +40,14 @@ class ATEntryOverviewController extends ControllerBase
 
     }
 
-    $html = '<a href="create-at-entry">'.$this->t("Create new assistive technology entry").'</a>';
+    $html = '<div class="at_entry_header_menu">
+        <a href="create-at-entry">'.$this->t("Add new assistive technology").'
+                    <i class="fa fa-plus" aria-hidden="true"></i> </a>
+             </div>';
+    foreach ($atEntries as $atEntry) {
 
-    $html.= "<table><tr>
-    <th scope='col'>Entry</th>
-    <th scope='col'>Languages</th>
-    <th scope='col'>Add Language</th>
-    <th scope='col'>Types</th>
-    <th scope='col'>Add Type</th>
-    <th scope='col'>Edit</th>
-</tr>";
-
-    foreach ($atEntries as $atEntry){
-
-      $html.=$this->renderATEntry($atEntry, $atDescriptionsOfATEntries[$atEntry->id()], $atPlatformsOfATEntries[$atEntry->id()] );
+        $html .= $this->renderATEntry($atEntry, $atDescriptionsOfATEntries[$atEntry->id()], $atPlatformsOfATEntries[$atEntry->id()]);
     }
-
-
-
-    $html.="</table>";
-
 
     $build = array(
       '#type' => 'markup',
@@ -77,52 +64,117 @@ class ATEntryOverviewController extends ControllerBase
 
   }
 
-  private function renderATEntry($atEntry, $atDescriptions, $atTypes){
+  private function renderATEntry($atEntry, $atDescriptions, $atTypes)
+  {
+    $html = '<div class="at_entry_container">
+<div class="at_entry_header">
+<h2>' . $atEntry->getTitle() . '</h2>
+<div class="at_entry_menu">
+ <a href="delete-at-entry/' . $atEntry->id() . '" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i><span class="sr-only">Delete</span> </a>
+ <a href="edit-at-entry/' . $atEntry->id() . '" title="Edit"><i class="fa fa-pencil" aria-hidden="true"></i><span class="sr-only">Edit</span></a>
 
-    $entry = $atEntry->getTitle();
+</div>
+ </div>
+';
 
-    $languages = "";
-    foreach ($atDescriptions as $atDescription){
-      $lang = $atDescription->field_at_description_language->getValue();
 
-      if($languages != ""){
-        $languages.=", ";
+    //Descriptions
+    $html .= '
+    <div class="at_entry_descriptions">
+        <div class="at_container_header">
+            <h3>Descriptions</h3>
+            <a href="create-description/' . $atEntry->id() . '">Add Description <i class="fa fa-plus" aria-hidden="true"></i> </a>
+    </div>
+
+    <div class="at_container_table">
+              <table class="table table-hover table-responsive ">
+          <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Language</th>
+               <th scope="col">Published</th>
+              <th scope="col">Draft</th>
+              <th scope="col">Delete</th>
+          </tr>';
+
+    foreach ($atDescriptions as $atDescription) {
+      $revision_ids = \Drupal::entityTypeManager()->getStorage('node')->revisionIds($atDescription);
+      $last_revision_id = end($revision_ids);
+
+      $draftTitle = "";
+      if ($atDescription->getRevisionId() != $last_revision_id) {
+        $draftTitle= $this->t("Edit revision");
+      }else{
+        $draftTitle = $this->t("Create new revision");
       }
-      $languages.='<a href="edit-description/'.$atDescription->id().'">'.$lang[0]['value'].'</a>';
-      $a = 1;
+      $mod = $atDescription->get('moderation_state')->getValue();
+      $published = "-";
+      if($mod[0]['value'] == 'draft'){
+        $draftTitle= $this->t("Edit draft");
+      }else{
+
+        $published = '<a href="view-description/'.$atDescription->id().'">'.$this->t("View published").'</a>';
+      }
+      $lang = $atDescription->field_at_description_language->getValue();
+      $html .= '<tr>
+                <td>'.$atDescription->getTitle().'</td>
+                <td>' . $lang[0]['value'] . '</td>
+                <td>'.$published.'</td>
+                <td><a href="edit-description/' . $atDescription->id() . '">' .$draftTitle . '</a></td>
+                <td><a href="delete-description/' . $atDescription->id() . '" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i><span class="sr-only">Delete</span></a></td>
+              </tr>';
+
     }
 
-    $manageLanguages = '<a href="create-description/'.$atEntry->id().'">Add language</a>';
+    $html .= '</table>
+    </div>
 
-    $types = "";
-    foreach ($atTypes as $atType){
-      if($types != ""){
-        $types.=", ";
-      }
+</div>';
+
+    //Types
+
+    $html .= '<div class="at_entry_types">
+    <div class="at_container_header">
+        <h3>Types</h3>
+        <a href="create-type/' . $atEntry->id() . '">Add Type <i class="fa fa-plus" aria-hidden="true"></i></a>
+    </div>
+    <div class="at_container_table">
+        <table class="table table-hover table-responsive ">
+            <tr>
+              <th scope="col">Type</th>
+              <th class="type_edit_header" scope="col">Edit</th>
+              <th class="type_delete_header" scope="col">Delete</th>
+            </tr>';
+    foreach ($atTypes as $atType) {
+      $html.="<tr>";
 
       $type = $atType->bundle();
-
-      if($type == "at_type_software"){
-        $types.='<a href="edit-type/'.$atType->id().'">'.$this->t("Software").'</a>';
-      }else if($type== "at_type_app"){
-        $types.='<a href="edit-type/'.$atType->id().'">'.$this->t("App").'</a>';
-      }else{
+      if ($type == "at_type_software") {
+        $html.= "<td>".$this->t("Software").'</td>';
+      } else if ($type == "at_type_app") {
+        $html.= "<td>".$this->t("App").'</td>';
+      } else {
         //browser_extension
-        $types.='<a href="edit-type/'.$atType->id().'">'.$this->t("Browser extension").'</a>';
+        $html.= "<td>".$this->t("Browser Extension").'</td>';
       }
+
+
+      $html.= '<td><a href="edit-type/' . $atType->id() . '" title="Edit"><i class="fa fa-pencil" aria-hidden="true"></i><span class="sr-only">Edit</span></a></td>';
+      $html.= '<td><a href="delete-type/' . $atType->id() . '" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i><span class="sr-only">Delete</span></a></td>';
+      $html.="</tr>";
 
 
     }
 
-    $manageTypes = '<a href="create-type/'.$atEntry->id().'">'.$this->t("Add type").'</a>';
+    $html .= '
+        </table>
+    </div>
 
-    $editEntry = '<a href="edit-at-entry/'.$atEntry->id().'">'.$this->t("Edit/Delete").'</a>';
+</div>
+</div>';
 
+    return $html;
 
-    return '<tr><td>'.$entry.'</td><td>'.$languages.'</td><td>'.$manageLanguages.'</td><td>'.$types.'</td><td>'.$manageTypes.'</td><td>'.$editEntry.'</td>';
-  }
-
-
+    }
 
 
 }
