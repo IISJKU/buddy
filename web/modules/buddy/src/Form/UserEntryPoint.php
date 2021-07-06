9c175a4dc48da5d7f013957c9120491afd92f7ad
Peter Heumader <peter.heumader@gmail.com>
2021-07-06 10:30:41 +0200

    ^merged

--- web/modules/buddy/src/Form/UserEntryPoint.php
+++ web/modules/buddy/src/Form/UserEntryPoint.php
@@ -129,14 +129,14 @@ class UserEntryPoint extends FormBase {
 
         // Facebook button
         $html .= '<div class="auth-option">';
-        $html .= '<a class="social-auth auth-link" href="/user/login/facebook">';
+        $html .= '<a class="social-auth auth-link" href="user/login/facebook">';
         $html .= '<img class="social-auth auth-icon" ';
         $html .= 'src="modules/buddy/img/social/facebook_logo.svg" alt="';
         $html .= $this->t('Authenticate through Facebook');
         $html .= '"></a></div>';
         // Google button
         $html .= '<div class="auth-option">';
-        $html .= '<a class="social-auth auth-link" href="/user/login/google">';
+        $html .= '<a class="social-auth auth-link" href="user/login/google">';
         $html .= '<img class="social-auth auth-icon" ';
         $html .= 'src="modules/buddy/img/social/google_logo.svg" alt="';
         $html .= $this->t('Authenticate through Google');
