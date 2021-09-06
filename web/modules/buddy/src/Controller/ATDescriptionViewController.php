514181f754fb6e605af6605cd78fe44aefa0b351
tomurillo <tomurillo@gmail.com>
2021-09-06 14:54:12 +0200

    Invoke node_view_alter on AT description listings

--- web/modules/buddy/src/Controller/ATDescriptionViewController.php
+++ web/modules/buddy/src/Controller/ATDescriptionViewController.php
@@ -4,6 +4,7 @@ namespace Drupal\buddy\Controller;
 use Drupal\buddy\Util\Util;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Link;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\image\Entity\ImageStyle;
@@ -13,24 +14,16 @@ use Drupal\node\NodeInterface;
 class ATDescriptionViewController extends ControllerBase
 {
 
+  public function ATDescriptionView(NodeInterface $description=NULL) {
 
-  public function ATDescriptionView(NodeInterface $description=NULL){
-
-
-
-    $user = \Drupal::currentUser();
     $title = $description->getTitle();
 
     $language = $description->field_at_description_language->getValue();
 
-
     $atDescription = $description->field_at_description->getValue();
-
 
     $image = $description->field_at_description_at_image->getValue();
     $styled_image_url = ImageStyle::load('medium')->buildUrl($description->field_at_description_at_image->entity->getFileUri());
-
-
 
     $downloadLink = $description->field_at_description_download->getValue();
 
@@ -44,17 +37,13 @@ class ATDescriptionViewController extends ControllerBase
 <div><img src="'.$styled_image_url.'" alt="Preview image"></div>
 <div>'.$downloadLinkHTML.'</div>';
 
-
     $backLink = Link::createFromRoute($this->t('Back'),'buddy.at_entry_overview',[],['attributes' => ['class' => 'btn btn-primary overview-button']])->toString()->getGeneratedLink();
 
     $html.='<br>'.$backLink;
 
-
-
     $revision_ids = \Drupal::entityTypeManager()->getStorage('node')->revisionIds($description);
     $last_revision_id = end($revision_ids);
 
-    $revisionLink = "";
     if ($description->getRevisionId() != $last_revision_id) {
       $revisionLink = Link::createFromRoute($this->t('Edit revision'),'buddy.description_edit_form',['description' => $description->id()],  ['attributes' => ['class' => 'btn btn-primary overview-button']])->toString()->getGeneratedLink();
     }else{
@@ -68,6 +57,22 @@ class ATDescriptionViewController extends ControllerBase
       '#markup' => $html,
       '#title' => $title,
     );
+
+    $display = EntityViewDisplay::create([
+      'targetEntityType' => 'node',
+      'bundle' => 'at_description',
+      'status' => TRUE,
+    ]);
+    foreach ($description->getFields() as $field) {
+      $display->setComponent($field->definition['field_name'], [
+        'type' => $field->options['type'],
+        'settings' => $field->options['settings'],
+      ]);
+    }
+
+    // Invoke all view_alter hook implementations
+    $module_handler = \Drupal::moduleHandler();
+    $module_handler->invokeAll('node_view_alter', [$build, $description, $display]);
 
     return $build;
 
