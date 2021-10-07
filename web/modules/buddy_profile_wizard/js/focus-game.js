f1e89e6ac67cb0c696d95f52addaaf576ed26562
Peter Heumader <peter.heumader@gmail.com>
2021-10-07 14:19:08 +0200

    ^focus game half finished

--- web/modules/buddy_profile_wizard/js/focus-game.js
+++ web/modules/buddy_profile_wizard/js/focus-game.js
@@ -5,10 +5,22 @@ class FocusGame extends GameScene {
     this.text = null;
 
     this.numberOfCups = 3;
+    this.numberOfShuffles = 10;
+    this.shuffleDuration = 1000;
     this.rowHeight = 150;
     this.cups = [];
+    this.distractions = [];
+    this.distractionDelayTimer = 1000;
+    this.currentDistractionCount = 0;
+    this.maxNumbersOfDistractions = 4;
+    this.maxDistractionAnimationTime = 5000;
+    this.coin = null;
     this.animatedCup1 = null;
     this.animatedCup2 = null;
+    this.cupFallHeight = 800;
+
+    this.shuffleSteps = [];
+
 
   }
 
@@ -19,20 +31,63 @@ class FocusGame extends GameScene {
 
     this.load.image('cup', 'modules/buddy_profile_wizard/assets/img/focus_game/cup.png');
     this.load.image('coin', 'modules/buddy_profile_wizard/assets/img/focus_game/coin.png');
+    this.load.spritesheet('mummy', 'modules/buddy_profile_wizard/assets/img/focus_game/mummy37x45.png', { frameWidth: 37, frameHeight: 45 });
 
-
+    this.load.audio('zombie',  'modules/buddy_profile_wizard/assets/sounds/focus_game/zombie.wav');
+    this.load.audio('wosh1',  'modules/buddy_profile_wizard/assets/sounds/focus_game/wosh1.mp3');
+    this.load.audio('wosh2',  'modules/buddy_profile_wizard/assets/sounds/focus_game/wosh2.mp3');
+    this.load.audio('cupImpact',  'modules/buddy_profile_wizard/assets/sounds/focus_game/cupImpact.mp3');
 
   }
 
   create() {
 
+    this.wosh1 = this.sound.add('wosh1');
+    this.wosh2 = this.sound.add('wosh1');
+    this.cupImpact = this.sound.add('cupImpact');
     this.createTitle(stringFactory.getString("memory_game_short_term_title"));
     let memoryGameShortTerm = this;
 
     this.graphics = this.add.graphics();
+    this.mummyAnimation = this.anims.create({
+      key: 'walk',
+      frames: this.anims.generateFrameNumbers('mummy'),
+      frameRate: 16
+    });
 
 
 
+
+
+    this.startGame();
+
+
+
+
+  }
+
+  startGame(){
+
+
+    this.clear();
+    this.setupCupsAndCoin();
+    this.createShuffles();
+    this.dropCups();
+  }
+
+  clear(){
+    if(this.coin){
+      this.coin.destroy();
+    }
+
+    for(let i=0; i < this.cups.length; i++){
+
+      this.cups[i].destroy();
+    }
+  }
+
+  setupCupsAndCoin(){
+    this.cups = [];
     for(let i=0; i < this.numberOfCups; i++){
 
       let cupPosition = this.getPositionOfCub(i);
@@ -43,42 +98,185 @@ class FocusGame extends GameScene {
 
     }
 
-
-    this.shuffleCups(0,2);
-    /*
-
-    this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
-
-    this.path = new Phaser.Curves.Path(500, 500);
+    this.winningCup = this.cups[Math.floor(Math.random() * this.numberOfCups)];
 
 
-    this.path.ellipseTo(150, 100, 0, 180, true,30);
+    this.coin = this.add.sprite(this.winningCup.x, this.winningCup.y+this.cupFallHeight, 'coin');
+    this.coin.setScale(0.4);
+    this.coin.setDepth(-1);
+  }
 
-    this.cup = this.add.sprite(50, 50, 'cup');
-    this.cup.setScale(0.4);
+  createShuffles(){
 
-    this.cup.setData('vector', new Phaser.Math.Vector2());
+    this.shuffleSteps = [];
 
-    this.moveEnabled = true;
-    let focusGame = this;
-    this.tweens.add({
-      targets: focusGame.cup,
-      z: 1,
-      ease: 'Sine.easeInOut',
-      duration: 1000,
-      repeat: 0,
-      onComplete: function (tween, targets) {
-        focusGame.moveEnabled = false;
+    for(let i=0; i< this.numberOfShuffles; i++){
+
+      let cup1 = Math.floor(Math.random() * this.numberOfCups);
+
+      let cup2 = cup1;
+
+      while(cup1 === cup2){
+        cup2 = Math.floor(Math.random() * this.numberOfCups);
       }
-    });
-
-    */
 
 
+      this.shuffleSteps.push([cup1,cup2]);
+
+    }
+
+    this.totalTime = this.numberOfShuffles *this.shuffleDuration;
+
+  }
+
+  createDistraction(){
+
+    this.currentDistractionCount++;
+    let distractionType = 0;
+    let direction = Math.floor(Math.random() *  2);
+
+    if(distractionType === 0){
+      let mummy = this.add.sprite(0, 300, 'mummy').setScale(2);
+      mummy.play({ key: 'walk', repeat: -1 });
+
+      let availableHeight = this.cameras.main.height-mummy.height;
+
+      mummy.y = Math.floor(Math.random() * availableHeight);
+      let mummyYPosition = Math.floor(Math.random() * availableHeight);
+
+      let focusGame = this;
+      if(direction === 0){
+        this.tweens.add({
+          targets: mummy,
+          x: this.cameras.main.width+mummy.width,
+          y: mummyYPosition,
+          ease: 'Linear',
+          duration: this.maxDistractionAnimationTime-Math.floor(Math.random() *  (this.maxDistractionAnimationTime/2)),
+          onComplete: function (tween, targets){
+
+            mummy.destroy();
+            focusGame.currentDistractionCount--;
+          }
+        });
+      }else{
+        mummy.x = this.cameras.main.width+mummy.width;
+        mummy.scaleX = -mummy.scaleX;
+        this.tweens.add({
+          targets: mummy,
+          x: -mummy.width,
+          y: mummyYPosition,
+          ease: 'Linear',
+          duration: this.maxDistractionAnimationTime-Math.floor(Math.random() * (this.maxDistractionAnimationTime/2)),
+          onComplete: function (tween, targets){
+            mummy.destroy();
+            focusGame.currentDistractionCount--;
+          }
+        });
+      }
+
+
+
+      if(!this.zombieSound){
+        this.zombieSound = this.sound.add('zombie');
+      }
+      this.zombieSound.play();
+      this.distractions.push(mummy);
+    }
+
+  }
+
+   dropCups(){
+
+    for(let i=0; i < this.cups.length; i++){
+
+      if(i === 0){
+        let focusGame = this;
+        this.tweens.add({
+          targets: this.cups[i],
+          props: {
+            y: {value: this.cups[i].y+this.cupFallHeight, duration: 1000, ease: 'Bounce.easeOut'}
+          },
+          onComplete: function (tween, targets) {
+            focusGame.coin.visible = false;
+            focusGame.nextMove();
+          }
+        });
+      }else{
+        this.tweens.add({
+          targets: this.cups[i],
+          props: {
+            y: {value: this.cups[i].y+this.cupFallHeight, duration: 1000, ease: 'Bounce.easeOut'}
+          }
+        });
+
+      }
+
+
+
+
+
+    }
+     this.time.addEvent({
+       delay: 200,                // ms
+       callback: function (){
+         this.cupImpact.play();
+       },
+       //args: [],
+       callbackScope: this,
+     });
+
+  }
+
+  nextMove(){
+
+    let nextMove = this.shuffleSteps.pop();
+    if(nextMove){
+      this.shuffleCups(nextMove[0],nextMove[1]);
+    }else{
+      this.coin.visible = true;
+      this.coin.setPosition(this.winningCup.x, this.winningCup.y);
+
+      for(let i=0; i < this.cups.length; i++){
+        this.cups[i].setInteractive();
+        let currentCup = this.cups[i];
+        let winningCup = this.winningCup;
+        let focusGame = this;
+        this.cups[i].on('pointerdown', function (pointer) {
+
+          if(currentCup === winningCup){
+            console.log("You win");
+          }else{
+            focusGame.tweens.add({
+              targets: currentCup,
+              props: {
+                y: {value: currentCup.y-200, duration: 1000, ease: 'Bounce.easeOut'}
+              }
+            });
+            console.log("You lose");
+          }
+
+          focusGame.tweens.add({
+            targets: winningCup,
+            props: {
+              y: {value: winningCup.y-200, duration: 1000, ease: 'Bounce.easeOut'}
+            },
+            onComplete: function (tween, targets) {
+             focusGame.startGame();
+            }
+          });
+
+          console.log(currentCup);
+
+        });
+      }
+    }
 
   }
 
   shuffleCups(cup1,cup2){
+
+    this.wosh1.play();
+
 
 
     let angle = Phaser.Math.Angle.Between(this.cups[cup1].x, this.cups[cup1].y,this.cups[cup2].x, this.cups[cup2].y);
@@ -100,10 +298,19 @@ class FocusGame extends GameScene {
       targets: focusGame.cups[cup1],
       z: 1,
       ease: 'Sine.easeInOut',
-      duration: 1000,
-      repeat: -1,
+      duration: focusGame.shuffleDuration,
+      repeat: 0,
       onComplete: function (tween, targets) {
-        focusGame.moveEnabled = false;
+
+        //Waiting timer is needed. Otherwise last update would not be called with final position
+        //This would lead to a missplacement of cups
+        focusGame.waitingTimer = focusGame.time.addEvent({
+          callback: focusGame.waitingTimerComplete,
+          callbackScope: focusGame,
+          delay: 10,
+          loop: false
+        });
+
       }
     });
 
@@ -116,11 +323,16 @@ class FocusGame extends GameScene {
       targets: focusGame.cups[cup2],
       z: 1,
       ease: 'Sine.easeInOut',
-      duration: 1000,
-      repeat: -1,
+      duration: focusGame.shuffleDuration,
+      repeat: 0,
     });
 
   }
+  waitingTimerComplete(){
+    this.moveEnabled = false;
+    this.nextMove();
+  }
+
 
   getPositionOfCub(cupNumber){
 
@@ -153,7 +365,7 @@ class FocusGame extends GameScene {
 
     }
 
-    let yPos= Math.floor((rowIndex+1)/layout.length)* this.rowHeight+this.cameras.main.centerY;
+    let yPos= Math.floor((rowIndex+1)/layout.length)* this.rowHeight+this.cameras.main.centerY-this.cupFallHeight;
     console.log(rowIndex,numberOfCupInRow);
     console.log(xPos,yPos);
 
@@ -185,16 +397,10 @@ class FocusGame extends GameScene {
 
   }
 
-  startGame(){
-    this.removeSound = this.sound.add('boing');
 
-
-
-  }
-
-
-  update ()
+  update (timestamp,delta)
   {
+
    // this.graphics.clear();
    // this.graphics.lineStyle(2, 0xffffff, 1);
 
@@ -203,6 +409,8 @@ class FocusGame extends GameScene {
 
     if(this.moveEnabled){
 
+      this.totalTime-=delta;
+      this.distractionDelayTimer-=delta;
       if(this.animatedCup1){
         let t = this.animatedCup1.z;
         let vec = this.animatedCup1.getData('vector');
@@ -224,19 +432,17 @@ class FocusGame extends GameScene {
       }
 
 
-      /*
 
-      let t = this.cup.z;
-      let vec = this.cup.getData('vector');
 
-      //  The vector is updated in-place
-      this.path.getPoint(t, vec);
+      if(this.distractionDelayTimer < 0){
+        if(this.currentDistractionCount < this.maxNumbersOfDistractions && this.totalTime > this.maxDistractionAnimationTime){
 
-      this.cup.setPosition(vec.x, vec.y);
 
-      this.cup.setDepth(this.cup.y);
+          this.createDistraction();
+          this.distractionDelayTimer = Math.floor(Math.random() * 3000);
+        }
+      }
 
-      */
     }
 
   }
