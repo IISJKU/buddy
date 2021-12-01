a6ae6cc45c5fbabee5ebe46498d9b64462ea6061
Peter Heumader <peter.heumader@gmail.com>
2021-12-01 10:29:58 +0100

    ^library improvements

--- web/modules/buddy/src/Form/UserATEntryInstallInstructionsForm.php
+++ web/modules/buddy/src/Form/UserATEntryInstallInstructionsForm.php
@@ -69,7 +69,7 @@ class UserATEntryInstallInstructionsForm extends FormBase
 
     $form['introduction_install'] = [
       '#type' => 'markup',
-      '#markup' => '<hr><h2>'.$this->t("How do you get this AT?").'</h2><div><strong>'.$this->t("This AT is available as:").'</strong></div>',
+      '#markup' => '<hr><h2>'.$this->t("How do you get this Tool?").'</h2><div><strong>'.$this->t("This tool is available as:").'</strong></div>',
       '#allowed_tags' => ['button', 'a', 'div','img','h2','h1','p','b','strong','hr'],
 
     ];
