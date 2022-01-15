a006a277d13d710130ab81406efce825dc1c33f2
Peter Heumader <peter.heumader@gmail.com>
2022-01-15 11:34:23 +0100

    ^ajax recommendations finished

--- web/modules/buddy/src/Form/ATRecommendationForm.php
+++ web/modules/buddy/src/Form/ATRecommendationForm.php
@@ -45,58 +45,18 @@ class ATRecommendationForm extends FormBase
     $oarsch = $form_state->get('oldRecommendations');
 
     if (!empty($recommendations)) {
+      $form['recommendations'] = [
+        '#type' => 'markup',
+        '#prefix' => "<div class='buddy_recommendations'>",
+        '#suffix' => "</div>",
+        '#allowed_tags' => ['div','h2'],
+      ];
       $maxResults = min(count($recommendations), BuddyRecommender::$maxNumberOfATEntries);
       for ($i = 0; $i < $maxResults; $i++) {
         $atEntryID = array_shift($recommendations);
 
-        $descriptions = Util::getDescriptionsOfATEntry($atEntryID);
-        $user = \Drupal::currentUser();
-        $supportCategories = Util::getSupportCategoriesOfAtEntry(Node::load($atEntryID));
-        $description = Util::getDescriptionForUser($descriptions,$user);
-        $languages = Util::getLanguagesOfDescriptions($descriptions);
-        $platforms = Util::getPlatformsOfATEntry(Node::load($atEntryID));
-        $content = Util::renderDescriptionTiles($description,$user,$languages,$platforms,$supportCategories);
-
-        $content = Util::renderDescriptionTiles2($description,$supportCategories,$platforms,$languages,false,2);
-
-        $entryForm = [];
-
-        $entryForm['content'] = [
-          '#type' => 'markup',
-          '#prefix' => "<div class='at_library_container'",
-          '#suffix' => '<div class="col-2">',
-          '#markup' => $content,
-          '#allowed_tags' => ['button', 'a', 'div', 'img','h3','h2', 'h1', 'p', 'b', 'b', 'strong', 'hr', 'ul', 'li', 'span'],
-        ];
-
-        $atRecord = Util::getATRecordOfATEntry($atEntryID);
-        $valueLabel = $this->getLabelForFavourite($atRecord);
-
-        $entryForm['at_favourites'] = [
-          '#name' => $atEntryID . "_" . $description->id(),
-          '#type' => 'submit',
-          '#button_type' => 'primary',
-          '#value' =>$valueLabel,
-          '#submit' => ['::favouriteSubmit'],
-          '#ajax' => array(
-            'callback' => '::favouriteAjaxCallback',
-            'wrapper' => "favourites_wrapper_".$atEntryID,
-          ),
-          '#prefix' => '<div id="favourites_wrapper_'.$atEntryID.'">',
-          '#suffix' => '</div>'
-        ];
-
-        $entryForm['close'] = [
-          '#type' => 'markup',
-           '#markup' => '</div></div></div></div>',
-          '#allowed_tags' => ['button', 'a', 'div', 'img','h3','h2', 'h1', 'p', 'b', 'b', 'strong', 'hr', 'ul', 'li', 'span'],
-        ];
-        $entryForm['at_favourites']['#attributes']['class'][] = 'buddy_link_button buddy_button';
-
-        $form[$atEntryID] = $entryForm;
-
+        $form['recommendations'][$atEntryID] = $this->renderATEntryForm($atEntryID);
         if(!$ajaxUpdate){
-
           $recommendations_all[] = $atEntryID;
         }
       }
@@ -130,6 +90,52 @@ class ATRecommendationForm extends FormBase
     }
 
      return $form;
+  }
+
+  private function renderATEntryForm($atEntryID){
+    $descriptions = Util::getDescriptionsOfATEntry($atEntryID);
+    $user = \Drupal::currentUser();
+    $supportCategories = Util::getSupportCategoriesOfAtEntry(Node::load($atEntryID));
+    $description = Util::getDescriptionForUser($descriptions,$user);
+    $languages = Util::getLanguagesOfDescriptions($descriptions);
+    $platforms = Util::getPlatformsOfATEntry(Node::load($atEntryID));
+    $content = Util::renderDescriptionTiles2($description,$supportCategories,$platforms,$languages,false,2);
+
+    $entryForm = [];
+
+    $entryForm['content'] = [
+      '#type' => 'markup',
+      '#prefix' => "<div class='at_library_container'",
+      '#suffix' => '<div class="col-2">',
+      '#markup' => $content,
+      '#allowed_tags' => ['button', 'a', 'div', 'img','h3','h2', 'h1', 'p', 'b', 'b', 'strong', 'hr', 'ul', 'li', 'span'],
+    ];
+
+    $atRecord = Util::getATRecordOfATEntry($atEntryID);
+    $valueLabel = $this->getLabelForFavourite($atRecord);
+
+    $entryForm['at_favourites'] = [
+      '#name' => $atEntryID . "_" . $description->id(),
+      '#type' => 'submit',
+      '#button_type' => 'primary',
+      '#value' =>$valueLabel,
+      '#submit' => ['::favouriteSubmit'],
+      '#ajax' => array(
+        'callback' => '::favouriteAjaxCallback',
+        'wrapper' => "favourites_wrapper_".$atEntryID,
+      ),
+      '#prefix' => '<div id="favourites_wrapper_'.$atEntryID.'">',
+      '#suffix' => '</div>'
+    ];
+
+    $entryForm['close'] = [
+      '#type' => 'markup',
+      '#markup' => '</div></div></div></div>',
+      '#allowed_tags' => ['button', 'a', 'div', 'img','h3','h2', 'h1', 'p', 'b', 'b', 'strong', 'hr', 'ul', 'li', 'span'],
+    ];
+    $entryForm['at_favourites']['#attributes']['class'][] = 'buddy_link_button buddy_button';
+
+    return $entryForm;
   }
 
   public function favouriteSubmit(array &$form, FormStateInterface $form_state) {
@@ -174,7 +180,7 @@ class ATRecommendationForm extends FormBase
   public function favouriteAjaxCallback(array &$form, FormStateInterface $form_state) {
     $arguments = explode("_", $form_state->getTriggeringElement()['#name']);
     $atEntryID = $arguments[0];
-    return $form[$atEntryID]['at_favourites'];
+    return $form['recommendations'][$atEntryID]['at_favourites'];
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state)
