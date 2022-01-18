b0b7c34fda3f251a041e15f601544e29ffb29489
Peter Heumader <peter.heumader@gmail.com>
2022-01-18 14:56:23 +0100

    ^styling fix started

--- web/modules/buddy/src/Form/UserProfilePreferencesForm.php
+++ web/modules/buddy/src/Form/UserProfilePreferencesForm.php
@@ -213,11 +213,11 @@ class UserProfilePreferencesForm extends FormBase
       '#markup' => '
   <div class="row">
     <div class="col-12 col-md-4 text-align-right profile-progress">
-      <b>'.$this->t("Progress").':</b>
+      <b id="progress_label">'.$this->t("Progress").':</b>
     </div>
-    <div class="col-12 col-md-8">
+    <div class="col-12 col-md-5">
       <div class="progress">
-        <div id="profile-progress-bar" class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        <div id="profile-progress-bar" aria-labelledby="progress_label" class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
         <span class="sr-only">0% complete</span>
       </div>
     </div>
@@ -226,8 +226,9 @@ class UserProfilePreferencesForm extends FormBase
 
 
     $prefixHTML = '<div class="buddy-user-profile-category-container"><div class="row">
-  <div class="col-12 col-lg-4 buddy-category-image">'.$currentCategory->field_at_category_user_descript->value.'</div>
-  <div class="col-12 col-lg-8"><h2>'.$currentCategory->field_at_category_user_title->value.'</h2>';
+  <div class="col-12 col-lg-2"></div>
+  <div class="col-12 col-lg-3 buddy-category-image">'.$currentCategory->field_at_category_user_descript->value.'</div>
+  <div class="col-12 col-lg-5"><h2>'.$currentCategory->field_at_category_user_title->value.'</h2>';
     if ($atCategoryContainer->field_category_container_user_de->value) {
 
       $form['category_container_' . $categoryContainerId]['container_description'] = array(
